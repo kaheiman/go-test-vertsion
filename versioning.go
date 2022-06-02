24d93a02aaff16987cf77f2ab20b9e99bff21c98
ka hei man <marcus.man@deepcodify.com>
2022-06-01 21:48:30 -0700

    added new api

--- versioning.go
+++ versioning.go
@@ -1,5 +1,14 @@
 package versioning
 
+type Address struct {
+	Address    string
+	PostalCode string
+}
+
 func Hello() string {
 	return "Hello, You!"
+}
+
+func Goodbye() string {
+	return "Goodbye for now!"
 }
